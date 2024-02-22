9384a86559f044ae033679e713c1bb697d136365
Mark LaBarbara <mlabarbara@gmail.com>
2024-02-22 14:44:12 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,13 +2,13 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Marks Kasm Repo',
+    description: 'This workspace was generated using Kasms site-registry template. I will use this to create and upload my custom Kasm images.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.io/Mlabarbara/',
+    contactUrl: 'https://github.com/Mlabarbara/kasm-registry/issues',
   },
-  reactStrictMode: true,
+  reactStrictMode: true ,
   swcMinify: true,
   basePath: '/kasm-registry/1.0',
   trailingSlash: true,
